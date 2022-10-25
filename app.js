2f304e57344a58f6efab75b1d5ab5fd11964907f
feryeong <feryeong@nc-and.com>
2022-10-26 07:47:03 +0900

    update leave event

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const Router = require('./routes')
 
 
 let rooms = {}
+let broadcast = []
 let users = []
 
 app.use(cors({
@@ -44,16 +45,20 @@ WSS.on("connection", (ws) => {
   users.push(ws)
   console.log(`${ws.id} 접속: 총 ${users.length}명, ${WSS.clients.length}명`)
   ws.send(JSON.stringify({type: "connected", data: `${ws.id}`}))
+
   ws.on('error', (err) => console.log(`${ws.id} error: ${err}`))
   ws.on('close', () => {
     if(ws.room) leaveRoom(ws)
+    leaveBroadcast(ws)
     const idx = users.findIndex(u => u.id === ws.id)
     users.splice(idx, 1)
     console.log(`${ws.id} 해제: 총 ${users.length}명`)
   })
+
   // 메시지 처리
   ws.on('message', (e) => {
-    const {type, data} = JSON.parse(e.toString())
+    const {type, data, sub} = JSON.parse(e.toString())
+    console.log('socket msg: ', type, data, sub)
     if(type === "join"){
       ws.room = data
       if(!rooms[data]) rooms[data] = [ws]
@@ -66,13 +71,31 @@ WSS.on("connection", (ws) => {
       }
     }
     if(type === "leave" && ws.room) {
-      leaveRoom(ws)
-      return toTheOther(ws, {type, data})
+      toTheOther(ws, {type, data})
+      return leaveRoom(ws)
     }
     if(type === "offer") return toTheOther(ws, {type, data})
     if(type === "answer") return toTheOther(ws, {type, data})
     if(type === "ice") return toTheOther(ws, {type, data})
     if(type === "peercontrol") return toTheOther(ws, {type, data})
+    // broadcast
+    if(type === "broadcast"){
+      if(data === "join"){
+        // 입장 체크
+        const index = broadcast.findIndex(w => w.id === ws.id)
+        if(index !== -1) {
+          broadcast.splice(index, 1)
+          return send(ws, {type: 'broadcast', data: 'error'})
+        }
+        // 정상 로직
+        broadcast.push(ws)
+        if(broadcast.length === 1) send(ws, {type: 'broadcast', data: 'host'})
+        else send(ws, {type: 'broadcast', data: 'guest'})
+        // finally
+        broadcastAll()
+      }
+      if(data === "leave") leaveBroadcast(ws)
+    }
   })
 })
 
@@ -83,8 +106,24 @@ function leaveRoom(ws){
   if(rooms[ws.room].length === 0) delete rooms[ws.room]
   delete ws.room
 }
+function send(ws, msg){
+  ws.send(JSON.stringify(msg))
+}
 function toTheOther(ws, msg){
   rooms[ws.room].forEach(u => u.id !== ws.id && u.send(JSON.stringify(msg)))
+}
+function leaveBroadcast(ws){
+  const idx = broadcast.findIndex(w => w.id === ws.id)
+  if(idx === -1) return
+  else broadcast.splice(idx, 1)
+  broadcastAll()
+}
+function broadcastAll(msg){
+  const data = {
+    host: broadcast[0].id,
+    particapants: broadcast.length
+  }
+  broadcast.forEach(w => send(w, {type: 'broadcast', data: 'all', sub: msg || data}))
 }
 /*
   wss.clients 는 리스트가 아닌 Set이므로, 개수를 셀 때 length 대신 size
